ce5c552fd86ef0bca6e5b215e89b7c141d9d3e6b
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-04-25 14:32:03 +0200

    Initial commit Rework of previous app. This time to get all of the code much simpler. Got points to show on screen.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-   kotlin("jvm") version "1.4.32"
-   id("org.openjfx.javafxplugin") version "0.0.9"
-
+	kotlin("jvm") version "1.4.32"
+	id("org.openjfx.javafxplugin") version "0.0.9"
+	
 }
 
 group = "me.thoma"
 version = "1.0-SNAPSHOT"
 
 repositories {
-   mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-   testImplementation(kotlin("test-junit"))
-   implementation("no.tornado:tornadofx:1.7.20")
-
+	testImplementation(kotlin("test-junit"))
+	implementation("no.tornado:tornadofx:1.7.20")
+	
 }
 
 javafx {
-   modules("javafx.controls", "javafx.fxml")
+	modules("javafx.controls", "javafx.fxml")
 }
 
 tasks.test {
-   useJUnit()
+	useJUnit()
 }
 
 tasks.withType<KotlinCompile>() {
-   kotlinOptions.jvmTarget = "1.8"
+	kotlinOptions.jvmTarget = "1.8"
 }
